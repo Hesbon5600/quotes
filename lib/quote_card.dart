e049b6bbf08a7a5d2cc7a120944b7490c329d15a
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2019-11-06 08:12:11 +0300

    simple quotes flutter app

---
+++ lib/quote_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'quotes.dart';
+
+class QuoteCard extends StatelessWidget {
+  final Quote quote;
+  final Function delete;
+  QuoteCard({this.quote, this.delete});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 18, color: Colors.grey[800]),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            FlatButton.icon(
+              onPressed: this.delete,
+              label: Text('delete quote'),
+              icon: Icon(Icons.delete),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
